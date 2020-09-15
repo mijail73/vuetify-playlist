eb625b039a82ff5877f6cbcee95ef7c64d180675
Mijail <mijail.iso@ciencias.unam.mx>
2020-09-15 16:48:51 -0500

    tutorial5

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 new Vue({
     el:'#vue-app',
     data : {
-        name: 'Mijail',
-        job: 'Intern',
-        website: 'http://google.com',
-        websiteTag: '<a href="http://google.com">The Google</a>'
+        // name: 'Mijail',
+        // job: 'Intern',
+        // website: 'http://google.com',
+        // websiteTag: '<a href="http://google.com">The Google</a>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(time) {
-            return 'Good ' + time + ' ' + this.name;
+        // greet: function(time) {
+        //     return 'Good ' + time + ' ' + this.name;
+        // }
+        add: function(inc) {
+            this.age += inc;
+        },
+        subtract: function(inc) {
+            this.age -= inc;
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
